2bb32d467909021ec0ba6029e72043872d4eeac4
Eunseo Wi <mymaywi@gmail.com>
2026-09-17 15:55:43 +0900

    style: center public footer credit

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,14 +1,6 @@
-import { Link } from "@tanstack/react-router";
-
 export function SiteFooter() {
   return (
-    <footer className="mx-auto flex w-full max-w-6xl flex-wrap items-center justify-between gap-3 px-4 pb-8 pt-12 sm:px-6">
-      <Link
-        to="/login"
-        className="text-xs font-medium text-swag-navy/70 underline-offset-4 hover:text-swag-navy hover:underline"
-      >
-        Staff &amp; Peer Mentor Login
-      </Link>
+    <footer className="mx-auto flex w-full max-w-6xl items-center justify-center px-4 pb-8 pt-12 text-center sm:px-6">
       <p className="text-xs text-swag-navy/60">website by @eunseowi</p>
     </footer>
   );
